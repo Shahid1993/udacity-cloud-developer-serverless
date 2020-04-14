36b3688dce70fa3371c002ab3196e730b6e7ed6b
Shahid1993 <shahid.bhat1994@gmail.com>
2020-04-14 13:42:35 +0530

    chore: Setup client for Lesson 5

--- exercises/lesson-5/client/src/config.ts
+++ exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://0vbsz3ptxd.execute-api.us-east-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-25c8tyzh.auth0.com',
+  clientId: 'VkpvR2sOfeep3PrEHQ3DZ444I0lVWK3i',
   callbackUrl: 'http://localhost:3000/callback'
 }
