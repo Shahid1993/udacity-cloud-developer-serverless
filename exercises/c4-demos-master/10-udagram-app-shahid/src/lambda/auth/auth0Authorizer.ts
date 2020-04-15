0f9c68afbce05d4ff5b24dff2d6ac4cac38ecad4
Shahid1993 <shahid.bhat1994@gmail.com>
2020-04-15 15:29:28 +0530

    feat: Store secrets in Secrets Manager

--- exercises/c4-demos-master/10-udagram-app-shahid/src/lambda/auth/auth0Authorizer.ts
+++ exercises/c4-demos-master/10-udagram-app-shahid/src/lambda/auth/auth0Authorizer.ts
@@ -1,14 +1,21 @@
 import { CustomAuthorizerHandler, CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
 
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const auth0Secret = process.env.AUTH_O_SECRET
+const secretId = process.env.AUTH_0_SECRET_ID
+const secretField = process.env.AUTH_0_SECRET_FIELD
+
+const client = new AWS.SecretsManager()
+
+// Cache secret if a Lambda instance is used
+let cachedSecret: string
 
 export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
-    const decodedToken = verifyToken( event.authorizationToken )
+    const decodedToken = await verifyToken( event.authorizationToken )
     console.log('User was authorized')
 
     return {
@@ -43,7 +50,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
   }
 }
 
-function verifyToken(authHeader: string): JwtToken {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader)
     throw new Error('No authorization header')
 
@@ -53,8 +60,21 @@ function verifyToken(authHeader: string): JwtToken {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  return verify( token, auth0Secret) as JwtToken
+  const secretObject: any = await getSecret()
+  const secret = secretObject[secretField]
 
-  
+  return verify( token, secret) as JwtToken  
 }
 
+async function getSecret(){
+  if (cachedSecret) return cachedSecret
+
+  const data = await client
+    .getSecretValue({
+      SecretId: secretId
+    })
+    .promise()
+
+    cachedSecret = data.SecretString
+    return JSON.parse(cachedSecret)
+}
